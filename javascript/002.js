e4c445fabfd331e1faa6569e311b0380b4ca3c3b
paullabkorea <paullabkorea@gmail.com>
2022-04-27 18:12:12 +0900

    4월 27일

---
+++ javascript/002.js
@@ -0,0 +1,4 @@
+document.getElementById("one").innerHTML = "hello <strong>world document</strong>";
+console.log("hello world console");
+window.alert("hello world alert");
+document.write("hello!!");
